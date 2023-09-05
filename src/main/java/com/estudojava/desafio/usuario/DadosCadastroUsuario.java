df0b84cb20a85f9b8ba44d392c137714d00f202d
tiago <tgribeiro@gmail.com>
2023-09-05 17:15:18 -0300

    feat: Adicionando validations aos códigos

--- src/main/java/com/estudojava/desafio/usuario/DadosCadastroUsuario.java
+++ src/main/java/com/estudojava/desafio/usuario/DadosCadastroUsuario.java
@@ -1,6 +1,31 @@
 package com.estudojava.desafio.usuario;
 
+import jakarta.validation.Valid;
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Pattern;
+
 import java.math.BigDecimal;
 
-public record DadosCadastroUsuario(String nome, String sobrenome, String cpf, String email, String senha, BigDecimal saldo, TipoDeUsuario tipoDeUsuario) {
+public record DadosCadastroUsuario(
+        @NotBlank
+        String nome,
+        @NotBlank
+        String sobrenome,
+        @NotBlank
+        @Pattern(regexp = "\\d{3}\\.?\\d{3}\\.?\\d{3}\\-?\\d{2}")
+        String cpf,
+        @NotBlank
+        @Email
+        String email,
+
+        @NotBlank
+        String senha,
+        @NotNull
+        BigDecimal saldo,
+
+        @NotNull
+        @Valid
+        TipoDeUsuario tipoDeUsuario) {
 }
